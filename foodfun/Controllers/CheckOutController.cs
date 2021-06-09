4be83393daceb45412a44fd822fb6f45deb35cd5
miwa <miwa.huang0625@gmail.com>
2021-06-09 15:33:28 +0800

    start

---
+++ foodfun/Controllers/CheckOutController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using foodfun.Models;
+
+namespace foodfun.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        GoPASTAEntities db;
+
+        public CheckOutController()
+        {
+            db = new GoPASTAEntities();
+        }
+
+        [AllowAnonymous]
+        public ActionResult MealService()
+        {
+            MealServiceViewModel model = new MealServiceViewModel()
+            {
+                mealServiceList = db.MealService.OrderBy(m => m.mealservice_no).ToList()
+
+            };
+            return View(model);
+
+        }
+
+        // GET: CheckOut
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult MealService(MealServiceViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                if (model.mealServiceList == null)
+                {
+                    model.mealServiceList = db.MealService.OrderBy(m => m.mealservice_no).ToList();
+                }
+                return View(model);
+            }
+
+            TempData["mealservice_no"] = model.mealservice_no;
+            if (model.mealservice_no == "A")
+            {
+                TempData["Table_no"] = model.Table_no;
+            }
+            else
+            {
+                TempData["receive_address"] = model.receive_address;
+            }
+
+            TempData["SchedulOrderTime"] = model.SchedulOrderTime;
+            return RedirectToAction("Confirmation");
+        }
+
+        public ActionResult Confirmation()
+        {
+            ConfirmationViewModel orderInfoView = new ConfirmationViewModel()
+            {
+                Order = new Orders()
+            };
+            Users userinfo = new Users();
+
+            userinfo = db.Users.Where(m => m.account_name == UserAccount.UserNo).FirstOrDefault();
+
+
+            if (UserAccount.IsLogin)
+            {
+                orderInfoView.Cart = db.Carts.Where(m => m.mno == UserAccount.UserNo).ToList();
+                orderInfoView.Order.mno = UserAccount.UserNo;
+                orderInfoView.Order.receive_name = userinfo.mname;
+                orderInfoView.Order.receive_phone = userinfo.phone;
+            }
+            else
+            {
+                orderInfoView.Cart = db.Carts.Where(m => m.cart_lotno == Cart.LotNo).ToList();
+            }
+            orderInfoView.Order.mealservice_no = TempData["mealservice_no"].ToString();
+            orderInfoView.Order.SchedulOrderTime = Convert.ToDateTime(TempData["SchedulOrderTime"]);
+
+            if (orderInfoView.Order.mealservice_no == "A")
+            {
+                orderInfoView.Order.table_no = TempData["Table_no"].ToString();
+
+            }
+            else
+            {
+                orderInfoView.Order.receive_address = TempData["receive_address"].ToString();
+
+            }
+
+
+            return View(orderInfoView);
+
+        }
+    }
+}
---
+++ foodfun/Models/ViewModel/ConfirmationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace foodfun.Models
+{
+    public class ConfirmationViewModel
+    {
+        public Orders Order { get; set; }
+        public IEnumerable<Carts> Cart { get; set; }
+
+
+    }
+}
