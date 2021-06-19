d354b01dca1a2708ef807032d278fca6b627e031
Doris Hsieh <shuyu.doris@gmail.com>
2021-06-19 10:35:38 +0800

    0619 後臺功能版面分成admin & staff

--- foodfun/App_Class/AppService.cs
+++ foodfun/App_Class/AppService.cs
@@ -40,28 +40,59 @@ public static class AppService
     /// <summary>
     /// 後台套版
     /// </summary>
-    public static List<ProgramList> GetModuleList()
+    /// <summary>
+    /// 後台套版
+    /// </summary>
+    public static List<ProgramList> GetModuleList(EnumList.LoginRole role)
     {
         using (GoPASTAEntities db = new GoPASTAEntities())
         {
-            return db.ProgramList
+            List<ProgramList> programLists;
+
+            if (role == EnumList.LoginRole.Staff)
+            {
+                programLists = db.ProgramList.Where(m => m.parentid == 0 &&
+               m.filter_role == "S").OrderBy(m => m.pro_no).ToList();
+            }
+            else
+            {
+                programLists = db.ProgramList
                 .Where(m => m.parentid == 0)
                 .OrderBy(m => m.pro_no)
                 .ToList();
+
+
+            }
+            return programLists;
+
         }
 
     }
-    public static List<ProgramList> GetProgramLists(int parentID)
+    public static List<ProgramList> GetProgramLists(int parentID, EnumList.LoginRole role)
     {
         using (GoPASTAEntities db = new GoPASTAEntities())
         {
-            return db.ProgramList
+            List<ProgramList> programLists;
+
+            if (role == EnumList.LoginRole.Staff)
+            {
+                programLists = db.ProgramList
+                .Where(m => m.parentid == parentID && m.filter_role == "S")
+                .OrderBy(m => m.pro_no)
+                .ToList();
+
+            }
+            else
+            {
+                programLists = db.ProgramList
                 .Where(m => m.parentid == parentID)
                 .OrderBy(m => m.pro_no)
                 .ToList();
+
+            }
+            return programLists;
         }
 
     }
-
 
 }
